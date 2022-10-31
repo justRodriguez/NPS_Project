5954985ab5f79723bb7c4ee013ed1613fda8b51f
Rodriguez <a.m.vivas.rodriguez@st.hanze.nl>
2022-10-31 14:34:15 +0100

    Update master.ps1

--- master.ps1
+++ master.ps1
@@ -1,4 +1,3 @@
-# source: https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-r2-and-2008/cc731207(v=ws.10)
 #----------------------------------------
 # NPS Server
 #----------------------------------------
