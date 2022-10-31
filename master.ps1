5a625fde8446552a0fdf78e0d275bf5bb7ee664c
Rodriguez <a.m.vivas.rodriguez@st.hanze.nl>
2022-10-31 14:33:56 +0100

    Update master.ps1

--- master.ps1
+++ master.ps1
@@ -1,9 +1,21 @@
+# source: https://learn.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-r2-and-2008/cc731207(v=ws.10)
 #----------------------------------------
 # NPS Server
 #----------------------------------------
 # Adds a Network Policy Server (NPS) to the list of registered server(s) in Active Directory
 add registeredserver domain="axians.local" server= "10.100.128.117" 
 
+
+#----------------------------------------
+# RADIUS Client
+#----------------------------------------
+add client name=                <name>
+           address=             <ip_address>
+           state=               enable/disable
+           sharedsecret=        <secret>
+           requireauthattrib=   yes/no
+           napcompatible=       yes/no
+           vendorname=          <v_name>
 
 
 #----------------------------------------
@@ -17,12 +29,15 @@ add crp name=             <name>
         profileid=        <id>
         advprofiletype=   <type>
         advprofiledata=   <data>
-        profiledata=      <data>
-        
-      
-
+        profiledata=      <data>        
 
 #----------------------------------------
 # Network Policy Commands
 #----------------------------------------
 netsh nps add np name= "name_of_policy" state = "enable"
+
+
+
+
+
+
