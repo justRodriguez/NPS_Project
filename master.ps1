2390b96f1efbb0b0c490a53891d668180453713d
Rodriguez <a.m.vivas.rodriguez@st.hanze.nl>
2022-10-31 13:03:08 +0100

    Create master.ps1

---
+++ master.ps1
@@ -0,0 +1,26 @@
+#----------------------------------------
+# NPS Server
+#----------------------------------------
+# Adds a Network Policy Server (NPS) to the list of registered server(s) in Active Directory
+add registeredserver domain="axians.local" server= "10.100.128.117" 
+
+
+
+------------------------------
+netsh nps add np name= "name_of_policy" state = "enable"
+
+
+#----------------------------------------
+# Connection Request Policy Commands
+#----------------------------------------
+add crp name=             <name>
+        state=            enable/disable
+        processingorder=  <order>
+        policysource=     <source>
+        conditionid=      <data>
+        profileid=        <id>
+        advprofiletype=   <type>
+        advprofiledata=   <data>
+        profiledata=      <data>
+        
+      
